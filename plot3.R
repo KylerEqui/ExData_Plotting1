0e691440a0d41e9c7519c6c16625705dbaef3147
Kyler Equi <kylerequi@gmail.com>
2016-05-29 16:03:29 -0400

    Second Commit

--- plot3.R
+++ plot3.R
@@ -11,12 +11,11 @@ plot_df$Sub_metering_3 <- as.numeric(as.character(plot_df$Sub_metering_3))
 
 plot_df <- transform(plot_df, Timestamp=as.POSIXct(paste(Date,Time)), "%d/%m/%Y %H:%M:%S")
 
-
 plot(plot_df$Timestamp, plot_df$Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering")
 lines(plot_df$Timestamp, plot_df$Sub_metering_2, col="red")
 lines(plot_df$Timestamp, plot_df$Sub_metering_3, col="blue")
-legend("topright", lty=1, col=c("black","red","blue"), 
-       c("Sub_metering_1  ","Sub_metering_2  ", "Sub_metering_3  "))
+legend("topright", col=c("black","red","blue"), c("Sub_metering_1     ","Sub_metering_2     ", "Sub_metering_3     "),
+       lty=c(1,1), lwd=c(1,1))
 
 dev.copy(png, file="plot3.png", width=480, height=480)
 dev.off()
